44c4fa6d0b72497455cd005111628cea67610404
saintjb <48727494+saintjb@users.noreply.github.com>
2020-05-11 14:49:00 +0300

    base

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+const  App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Technologies />
+  
     </div>
   );
 }
+
+const Technologies = () => {
+  return(
+    <div>
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+    </div>
+  );
+}
+
+const  Header = () => {
+  return (
+    <div>
+      <p>Hello someone</p>
+      <a href='#s'>Home </a>
+      <br/>
+      <a href='#s'>News feed </a>
+      <br/>
+      <a href='#s'>Messages </a>
+    </div>
+  );
+}
+
+
 
 export default App;
